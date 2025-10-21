f6d519d723073ba77c2492f8f018aa129c9213a5
PLLN <pl.lakshminarasimhan@gmail.com>
2025-10-21 21:29:43 +0530

    organize resources

---
+++ js/iterators_fff.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.log('welcome iterators!');
+
+const dragons = [
+    'cool dragon',
+    'angry dragon',
+    'nasty dragon',
+];
+
+for (const dragon of dragons) {
+    console.log(dragon);
+}
